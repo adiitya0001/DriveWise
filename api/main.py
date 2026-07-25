7aace98de6c39684dc828a73bc29a605ac7b0765
adiitya0001 <adityar00001@gmail.com>
2026-07-26 00:27:41 +0530

    Fix production CORS and update DriveWise branding

--- api/main.py
+++ api/main.py
@@ -33,7 +33,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://drive-wise-5tanbsawc-adiitya0001s-projects.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
